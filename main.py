4dade63e3da937c5c7e6c606858f8dae0523c372
dxadkwai <xaminhdang@gmail.com>
2026-04-12 21:05:48 +0700

    update

---
+++ main.py
@@ -0,0 +1,91 @@
+from turtle import Screen
+import time
+
+from scoreboard import Scoreboard
+from snake import Snake
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard=Scoreboard()
+
+
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    #Detect Collision with Food
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+
+screen.exitonclick()
+
+
+
+
+
+
+
+#     segment_1=Turtle("square")
+#     segment_1.color("white")
+#
+#
+# segment_2=Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20,0)
+#
+# segment_3=Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40,0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+
+
+    def move (self):
+        for seg_num in range(len(self.segments)-1,0,-1):
+            x = self.segments[seg_num - 1].xcor()
+            y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x,y)
+        self.segments[0].forward(move_distance)
+
+    def create_snake(self):
+        for position in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+
+    def up(self):
+        if self.segments[0].heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.segments[0].heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def left(self):
+        if self.segments[0].heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+    def right(self):
+        if self.segments[0].heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
