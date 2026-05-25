5201f91461a9514675d16b2f8d57ed24d04dfb34
dxadkwai <xaminhdang@gmail.com>
2026-05-25 13:49:48 +0700

    string manipulation

---
+++ string.java
@@ -0,0 +1,56 @@
+public class string {
+    public static void main (String args[]) {
+        // String morning = "Good morning";
+        // System.out.println(morning);
+
+        // String night = new String("Goodnight");
+        // night = "Bad Night";
+        // System.out.println(night);
+
+        // String first_name = "Dang";
+        // String last_name = "XA";
+        // String full_name = first_name+last_name;
+        // int age = 18;
+        // String bio = "I am " + full_name + " and " +age+ " years old.";
+        // System.out.println(bio);
+
+        // String string1 = "Java";
+        // String string2 = "Java";
+        // String string3 = "java";
+        // System.out.println("Compare equal for string 1 and string 2: " + (string1==string2));
+        // System.out.println("Compare equal for string 1 and string 3: "+ string1.equals(string3));
+        // System.out.println("Compare equal for string 2 to string 3: " + string2.equalsIgnoreCase(string3));
+        // String w = "I am a strong man";
+        // String newW = w.replace("o","a");
+        // String word1 = w.substring (0, 11);
+        // System.out.println(word1);
+        // System.out.println(newW);
+        // String name = "Alice";
+        // int age = 18;
+        // //%s is string
+        // //%d is integer
+        // String message = String.format("Hello, %s! You are %d years old", name, age);
+        // System.out.println(message);
+
+        // double price = 1234.567;
+        // String formattedPrice = String.format("The price is $%.2f", price);
+        // System.out.print(formattedPrice);
+
+
+        
+
+
+
+
+    }
+}
+
+
+// lenght() is used to find the length of a string by counting the number 
+// of characters, including spaces and everything
+// charAt() will find the character at a specific index number inside a string
+//string1.equals(str2) is comparing 2 string together
+//(str1==str2) will return a boolean.
+//equalsIgnoreCase() --> ignore case
+//.replace(""",""") --> replacing a char with another char
+//toUpperCase;toLowerCase()
