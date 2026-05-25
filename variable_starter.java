ab6e346695586c5b6091e9454158708c8e7a60f5
dxadkwai <xaminhdang@gmail.com>
2026-05-25 11:15:53 +0700

    java variables lesson

---
+++ variable_starter.java
@@ -0,0 +1,41 @@
+public class variable_starter {
+    public static void main (String args[]) {
+        // int Number = 20; //declaring and valueing variables
+        // System.out.println(Number);
+        // int b = 100;
+        // int sum = Number +b;
+        // System.out.println("The sum is "+sum);
+        // int NumInt = 10;
+        // System.out.println(NumInt);
+        // double numDouble = NumInt; // implicit casting to widen the data type
+        // System.out.println(numDouble);
+
+        // double pi = 3.14159265;
+        // System.out.println(pi);
+        // int pi_short = (int) pi; //explicit casting, contains parentheses
+        // System.out.print(pi_short);
+
+        double shapeValue = 50.65;
+        System.out.println("Double is "+shapeValue);
+        int shapeValue_int = (int) shapeValue;
+        System.out.println("Integer is "+shapeValue_int);
+        long shapeValue_long = (long) shapeValue;
+        System.out.println("Long is "+shapeValue_long);
+        float shapeValue_float = (float) shapeValue;
+        System.out.println("Float is "+shapeValue_float);
+
+
+
+
+    }
+}
+
+//data types
+//byte: 8 bits of data - hold short number -->2^8= 256 numbers - from (-128 --> 127)
+//short: 16 bits of data - medium number or data--> 2^16 numbers
+//int: 32 bits
+//if its too big, use data type LONG, and have to add the letter L at the end of the num
+//float,double for floating point number
+//data has to be added letter F afterwards if its float
+//can declare multiple variables at once, but they have to be the same data type on one line
+
