a0156c46143b1ebadfcc49d01bc5d1aff36e1777
dxadkwai <xaminhdang@gmail.com>
2026-06-01 13:26:05 +0700

    add new leetcode questions

---
+++ leetcode/FirstOccurence.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class FirstOccurence {
+    
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
---
+++ leetcode/TwoSum.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+
+public class TwoSum {
+    public int[] TwoSum(int[] nums, int target) {
+        boolean option = false;
+        for (int i = 0; i < nums.length; i++) {
+            int a = target - nums[i];
+            for (int j = i+1; j<nums.length;j++) {
+                if (a==nums[j]) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};   
+    }
+}
